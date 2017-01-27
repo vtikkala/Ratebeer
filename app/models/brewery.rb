53278284f49b9fd939d439d566a0f0070ed543d7
Ville Tikkala <ville.tikkala@helsinki.fi>
2017-01-27 17:32:53 +0200

    Tehtävä 2 valmis

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,15 @@
 class Brewery < ActiveRecord::Base
 	has_many :beers
+
+	def print_report
+		puts self.name
+		puts "established at year #{self.year}"
+		puts "number of beers #{self.beers.count}"
+	end
+
+	def restart
+		self.year = 2017
+		puts "changed year to #{year}"
+	end
+
 end
