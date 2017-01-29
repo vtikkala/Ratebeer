aa26b5a7855068b0f1f9bece958428922bac503a
Ville Tikkala <ville.tikkala@helsinki.fi>
2017-01-29 17:17:32 +0200

    Viikko 2, tehtävä 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,18 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		rating_sum = 0
+		number_of_ratings = 0
+		average_rating = 0.0
+
+		self.ratings.each do |rating|
+			rating_sum = rating_sum + rating.score
+			number_of_ratings = number_of_ratings + 1
+		end
+
+		average_rating = rating_sum / number_of_ratings
+		return average_rating
+	end
 end
