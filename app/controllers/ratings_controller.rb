22c2b344644b0a3b74832c7866077eca7b6ef0df
Ville Tikkala <ville.tikkala@helsinki.fi>
2017-02-03 17:51:09 +0200

    Viikko 2, tehtävä 7

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,10 +9,10 @@ class RatingsController < ApplicationController
 
   def new
     @rating = Rating.new
+    @beers = Beer.all
   end
 
   def create
-    raise
     Rating.create params.require(:rating).permit(:score, :beer_id)
     redirect_to ratings_path
   end
